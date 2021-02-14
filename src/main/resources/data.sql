4d8035ec1d07f0430d635709757124d9c4aa37b2
Sparsh Verma <sparshvermadmc@gmail.com>
2021-02-14 22:13:23 +0530

    Category,Recipe and UnitOfMeasure repository added.

---
+++ src/main/resources/data.sql
@@ -0,0 +1,9 @@
+INSERT INTO category (description) VALUES ('American');
+INSERT INTO category (description) VALUES ('Italian');
+INSERT INTO category (description) VALUES ('Mexican');
+INSERT INTO category (description) VALUES ('Fast Food');
+INSERT INTO UNIT_OF_MEASURE (uom) VALUES ('Teaspoon');
+INSERT INTO UNIT_OF_MEASURE (uom) VALUES ('Tablespoon');
+INSERT INTO UNIT_OF_MEASURE (uom) VALUES ('Cup');
+INSERT INTO UNIT_OF_MEASURE (uom) VALUES ('Pinch');
+INSERT INTO UNIT_OF_MEASURE (uom) VALUES ('Ounce');
